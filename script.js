61e2918a11feea7114415e6b027d934fa01a4197
ATallSteve <atallsteve@gmail.com>
2023-01-26 13:49:47 +0100

    Exercise 54

--- script.js
+++ script.js
@@ -28,3 +28,31 @@ document.getElementById("array-ex5").innerHTML = "The array has length " + beatl
 // Exercise 6
 beatles.sort()
 document.getElementById("array-ex6").innerHTML = beatles
+
+//Exercise 52
+let myArray = ['Apples', 'Bananas', 'Coconuts', 'Dates']
+myArray[2] = 'Cheese'
+myArray.push('Crackers')
+console.log(myArray)
+// ['Apples', 'Bananas', 'Cheese', 'Dates', 'Crackers'] (myArray[2] so 
+// Coconuts gets replaced by Cheese and Crackers gets added at the end)
+
+//Exercise 53
+myArray.sort()
+console.log(myArray)
+// ['Apples', 'Bananas', 'Cheese', 'Crackers', 'Dates']
+//It gets alphabetically sorted
+
+//Exercise 54
+let text = `<ul>`
+myArray.forEach(mysteryFunction)
+text += `</ul>`
+document.getElementById('solutions').innerHTML = text
+
+function mysteryFunction(string) {
+    text += `<li> ${string} </li> `
+}
+console.log(text)
+// It creates an HTML list of the items of the array
+
+//Exercise 55
